4b683ff8e792159a3ac97fdf6779292ee012271c
GuyInTie <34623054+GuyInTie@users.noreply.github.com>
2024-06-15 04:09:25 +0300

    Update App.vue

--- frontend-VueJs/src/App.vue
+++ frontend-VueJs/src/App.vue
@@ -17,7 +17,520 @@ onMounted(async () => {
     console.log(err);
   }
 });
-
+// const sidebar = document.querySelector('.left-section')
+// const sidebarItems = document.querySelectorAll('.sidebar .item')
+// const catItems = document.querySelectorAll('.categories a')
+//
+// // document.getElementById('homeButton').addEventListener('click', function() {
+// //   window.location.href = 'index.html'; // Переход на главную страницу
+// // });
+// //
+// // document.getElementById('albumButton').addEventListener('click', function() {
+// //   window.location.href = 'album.html'; // Переход на страницу контактов
+// // });
+//
+// // выделение элемента по нажатию
+// sidebarItems.forEach((sideItem) => {
+//   sideItem.addEventListener('click', () => {
+//     sidebarItems.forEach((item) => {
+//       item.classList.remove('active')
+//     })
+//     sideItem.classList.add('active')
+//   })
+// })
+//
+// // для маленьких девайсов
+// window.addEventListener('scroll', () => {
+//   if (window.innerWidth <= 992) {
+//     if (window.pageYOffset > 20) {
+//       sidebar.style.paddingTop = '20px'
+//     } else {
+//       sidebar.style.paddingTop = '70px'
+//     }
+//   }
+// })
+//
+// // выделение нужной категории при нужной странице
+// document.addEventListener('DOMContentLoaded', () => {
+//   const currentPath = window.location.pathname
+//   console.log('Current Path:', currentPath) // отображение страницы текущей
+//
+//   sidebarItems.forEach((item) => {
+//     item.classList.remove('active')
+//   })
+//
+//   if (currentPath.includes('album.html')) {
+//     const popularTab = document.querySelector('.sidebar .item:nth-child(3)')
+//     console.log('Popular Tab:', popularTab)
+//     if (popularTab) {
+//       popularTab.classList.add('active')
+//       console.log('Popular Tab Activated')
+//     }
+//   } else if (currentPath.includes('index.html') || currentPath === '/') {
+//     const homeTab = document.querySelector('.sidebar .item:nth-child(1)')
+//     console.log('Home Tab:', homeTab)
+//     if (homeTab) {
+//       homeTab.classList.add('active')
+//       console.log('Home Tab Activated')
+//     }
+//   }
+// })// const sidebar = document.querySelector('.left-section')
+// const sidebarItems = document.querySelectorAll('.sidebar .item')
+// const catItems = document.querySelectorAll('.categories a')
+//
+// // document.getElementById('homeButton').addEventListener('click', function() {
+// //   window.location.href = 'index.html'; // Переход на главную страницу
+// // });
+// //
+// // document.getElementById('albumButton').addEventListener('click', function() {
+// //   window.location.href = 'album.html'; // Переход на страницу контактов
+// // });
+//
+// // выделение элемента по нажатию
+// sidebarItems.forEach((sideItem) => {
+//   sideItem.addEventListener('click', () => {
+//     sidebarItems.forEach((item) => {
+//       item.classList.remove('active')
+//     })
+//     sideItem.classList.add('active')
+//   })
+// })
+//
+// // для маленьких девайсов
+// window.addEventListener('scroll', () => {
+//   if (window.innerWidth <= 992) {
+//     if (window.pageYOffset > 20) {
+//       sidebar.style.paddingTop = '20px'
+//     } else {
+//       sidebar.style.paddingTop = '70px'
+//     }
+//   }
+// })
+//
+// // выделение нужной категории при нужной странице
+// document.addEventListener('DOMContentLoaded', () => {
+//   const currentPath = window.location.pathname
+//   console.log('Current Path:', currentPath) // отображение страницы текущей
+//
+//   sidebarItems.forEach((item) => {
+//     item.classList.remove('active')
+//   })
+//
+//   if (currentPath.includes('album.html')) {
+//     const popularTab = document.querySelector('.sidebar .item:nth-child(3)')
+//     console.log('Popular Tab:', popularTab)
+//     if (popularTab) {
+//       popularTab.classList.add('active')
+//       console.log('Popular Tab Activated')
+//     }
+//   } else if (currentPath.includes('index.html') || currentPath === '/') {
+//     const homeTab = document.querySelector('.sidebar .item:nth-child(1)')
+//     console.log('Home Tab:', homeTab)
+//     if (homeTab) {
+//       homeTab.classList.add('active')
+//       console.log('Home Tab Activated')
+//     }
+//   }
+// })// const sidebar = document.querySelector('.left-section')
+// const sidebarItems = document.querySelectorAll('.sidebar .item')
+// const catItems = document.querySelectorAll('.categories a')
+//
+// // document.getElementById('homeButton').addEventListener('click', function() {
+// //   window.location.href = 'index.html'; // Переход на главную страницу
+// // });
+// //
+// // document.getElementById('albumButton').addEventListener('click', function() {
+// //   window.location.href = 'album.html'; // Переход на страницу контактов
+// // });
+//
+// // выделение элемента по нажатию
+// sidebarItems.forEach((sideItem) => {
+//   sideItem.addEventListener('click', () => {
+//     sidebarItems.forEach((item) => {
+//       item.classList.remove('active')
+//     })
+//     sideItem.classList.add('active')
+//   })
+// })
+//
+// // для маленьких девайсов
+// window.addEventListener('scroll', () => {
+//   if (window.innerWidth <= 992) {
+//     if (window.pageYOffset > 20) {
+//       sidebar.style.paddingTop = '20px'
+//     } else {
+//       sidebar.style.paddingTop = '70px'
+//     }
+//   }
+// })
+//
+// // выделение нужной категории при нужной странице
+// document.addEventListener('DOMContentLoaded', () => {
+//   const currentPath = window.location.pathname
+//   console.log('Current Path:', currentPath) // отображение страницы текущей
+//
+//   sidebarItems.forEach((item) => {
+//     item.classList.remove('active')
+//   })
+//
+//   if (currentPath.includes('album.html')) {
+//     const popularTab = document.querySelector('.sidebar .item:nth-child(3)')
+//     console.log('Popular Tab:', popularTab)
+//     if (popularTab) {
+//       popularTab.classList.add('active')
+//       console.log('Popular Tab Activated')
+//     }
+//   } else if (currentPath.includes('index.html') || currentPath === '/') {
+//     const homeTab = document.querySelector('.sidebar .item:nth-child(1)')
+//     console.log('Home Tab:', homeTab)
+//     if (homeTab) {
+//       homeTab.classList.add('active')
+//       console.log('Home Tab Activated')
+//     }
+//   }
+// })// const sidebar = document.querySelector('.left-section')
+// const sidebarItems = document.querySelectorAll('.sidebar .item')
+// const catItems = document.querySelectorAll('.categories a')
+//
+// // document.getElementById('homeButton').addEventListener('click', function() {
+// //   window.location.href = 'index.html'; // Переход на главную страницу
+// // });
+// //
+// // document.getElementById('albumButton').addEventListener('click', function() {
+// //   window.location.href = 'album.html'; // Переход на страницу контактов
+// // });
+//
+// // выделение элемента по нажатию
+// sidebarItems.forEach((sideItem) => {
+//   sideItem.addEventListener('click', () => {
+//     sidebarItems.forEach((item) => {
+//       item.classList.remove('active')
+//     })
+//     sideItem.classList.add('active')
+//   })
+// })
+//
+// // для маленьких девайсов
+// window.addEventListener('scroll', () => {
+//   if (window.innerWidth <= 992) {
+//     if (window.pageYOffset > 20) {
+//       sidebar.style.paddingTop = '20px'
+//     } else {
+//       sidebar.style.paddingTop = '70px'
+//     }
+//   }
+// })
+//
+// // выделение нужной категории при нужной странице
+// document.addEventListener('DOMContentLoaded', () => {
+//   const currentPath = window.location.pathname
+//   console.log('Current Path:', currentPath) // отображение страницы текущей
+//
+//   sidebarItems.forEach((item) => {
+//     item.classList.remove('active')
+//   })
+//
+//   if (currentPath.includes('album.html')) {
+//     const popularTab = document.querySelector('.sidebar .item:nth-child(3)')
+//     console.log('Popular Tab:', popularTab)
+//     if (popularTab) {
+//       popularTab.classList.add('active')
+//       console.log('Popular Tab Activated')
+//     }
+//   } else if (currentPath.includes('index.html') || currentPath === '/') {
+//     const homeTab = document.querySelector('.sidebar .item:nth-child(1)')
+//     console.log('Home Tab:', homeTab)
+//     if (homeTab) {
+//       homeTab.classList.add('active')
+//       console.log('Home Tab Activated')
+//     }
+//   }
+// })// const sidebar = document.querySelector('.left-section')
+// const sidebarItems = document.querySelectorAll('.sidebar .item')
+// const catItems = document.querySelectorAll('.categories a')
+//
+// // document.getElementById('homeButton').addEventListener('click', function() {
+// //   window.location.href = 'index.html'; // Переход на главную страницу
+// // });
+// //
+// // document.getElementById('albumButton').addEventListener('click', function() {
+// //   window.location.href = 'album.html'; // Переход на страницу контактов
+// // });
+//
+// // выделение элемента по нажатию
+// sidebarItems.forEach((sideItem) => {
+//   sideItem.addEventListener('click', () => {
+//     sidebarItems.forEach((item) => {
+//       item.classList.remove('active')
+//     })
+//     sideItem.classList.add('active')
+//   })
+// })
+//
+// // для маленьких девайсов
+// window.addEventListener('scroll', () => {
+//   if (window.innerWidth <= 992) {
+//     if (window.pageYOffset > 20) {
+//       sidebar.style.paddingTop = '20px'
+//     } else {
+//       sidebar.style.paddingTop = '70px'
+//     }
+//   }
+// })
+//
+// // выделение нужной категории при нужной странице
+// document.addEventListener('DOMContentLoaded', () => {
+//   const currentPath = window.location.pathname
+//   console.log('Current Path:', currentPath) // отображение страницы текущей
+//
+//   sidebarItems.forEach((item) => {
+//     item.classList.remove('active')
+//   })
+//
+//   if (currentPath.includes('album.html')) {
+//     const popularTab = document.querySelector('.sidebar .item:nth-child(3)')
+//     console.log('Popular Tab:', popularTab)
+//     if (popularTab) {
+//       popularTab.classList.add('active')
+//       console.log('Popular Tab Activated')
+//     }
+//   } else if (currentPath.includes('index.html') || currentPath === '/') {
+//     const homeTab = document.querySelector('.sidebar .item:nth-child(1)')
+//     console.log('Home Tab:', homeTab)
+//     if (homeTab) {
+//       homeTab.classList.add('active')
+//       console.log('Home Tab Activated')
+//     }
+//   }
+// })// const sidebar = document.querySelector('.left-section')
+// const sidebarItems = document.querySelectorAll('.sidebar .item')
+// const catItems = document.querySelectorAll('.categories a')
+//
+// // document.getElementById('homeButton').addEventListener('click', function() {
+// //   window.location.href = 'index.html'; // Переход на главную страницу
+// // });
+// //
+// // document.getElementById('albumButton').addEventListener('click', function() {
+// //   window.location.href = 'album.html'; // Переход на страницу контактов
+// // });
+//
+// // выделение элемента по нажатию
+// sidebarItems.forEach((sideItem) => {
+//   sideItem.addEventListener('click', () => {
+//     sidebarItems.forEach((item) => {
+//       item.classList.remove('active')
+//     })
+//     sideItem.classList.add('active')
+//   })
+// })
+//
+// // для маленьких девайсов
+// window.addEventListener('scroll', () => {
+//   if (window.innerWidth <= 992) {
+//     if (window.pageYOffset > 20) {
+//       sidebar.style.paddingTop = '20px'
+//     } else {
+//       sidebar.style.paddingTop = '70px'
+//     }
+//   }
+// })
+//
+// // выделение нужной категории при нужной странице
+// document.addEventListener('DOMContentLoaded', () => {
+//   const currentPath = window.location.pathname
+//   console.log('Current Path:', currentPath) // отображение страницы текущей
+//
+//   sidebarItems.forEach((item) => {
+//     item.classList.remove('active')
+//   })
+//
+//   if (currentPath.includes('album.html')) {
+//     const popularTab = document.querySelector('.sidebar .item:nth-child(3)')
+//     console.log('Popular Tab:', popularTab)
+//     if (popularTab) {
+//       popularTab.classList.add('active')
+//       console.log('Popular Tab Activated')
+//     }
+//   } else if (currentPath.includes('index.html') || currentPath === '/') {
+//     const homeTab = document.querySelector('.sidebar .item:nth-child(1)')
+//     console.log('Home Tab:', homeTab)
+//     if (homeTab) {
+//       homeTab.classList.add('active')
+//       console.log('Home Tab Activated')
+//     }
+//   }
+// })// const sidebar = document.querySelector('.left-section')
+// const sidebarItems = document.querySelectorAll('.sidebar .item')
+// const catItems = document.querySelectorAll('.categories a')
+//
+// // document.getElementById('homeButton').addEventListener('click', function() {
+// //   window.location.href = 'index.html'; // Переход на главную страницу
+// // });
+// //
+// // document.getElementById('albumButton').addEventListener('click', function() {
+// //   window.location.href = 'album.html'; // Переход на страницу контактов
+// // });
+//
+// // выделение элемента по нажатию
+// sidebarItems.forEach((sideItem) => {
+//   sideItem.addEventListener('click', () => {
+//     sidebarItems.forEach((item) => {
+//       item.classList.remove('active')
+//     })
+//     sideItem.classList.add('active')
+//   })
+// })
+//
+// // для маленьких девайсов
+// window.addEventListener('scroll', () => {
+//   if (window.innerWidth <= 992) {
+//     if (window.pageYOffset > 20) {
+//       sidebar.style.paddingTop = '20px'
+//     } else {
+//       sidebar.style.paddingTop = '70px'
+//     }
+//   }
+// })
+//
+// // выделение нужной категории при нужной странице
+// document.addEventListener('DOMContentLoaded', () => {
+//   const currentPath = window.location.pathname
+//   console.log('Current Path:', currentPath) // отображение страницы текущей
+//
+//   sidebarItems.forEach((item) => {
+//     item.classList.remove('active')
+//   })
+//
+//   if (currentPath.includes('album.html')) {
+//     const popularTab = document.querySelector('.sidebar .item:nth-child(3)')
+//     console.log('Popular Tab:', popularTab)
+//     if (popularTab) {
+//       popularTab.classList.add('active')
+//       console.log('Popular Tab Activated')
+//     }
+//   } else if (currentPath.includes('index.html') || currentPath === '/') {
+//     const homeTab = document.querySelector('.sidebar .item:nth-child(1)')
+//     console.log('Home Tab:', homeTab)
+//     if (homeTab) {
+//       homeTab.classList.add('active')
+//       console.log('Home Tab Activated')
+//     }
+//   }
+// })// const sidebar = document.querySelector('.left-section')
+// const sidebarItems = document.querySelectorAll('.sidebar .item')
+// const catItems = document.querySelectorAll('.categories a')
+//
+// // document.getElementById('homeButton').addEventListener('click', function() {
+// //   window.location.href = 'index.html'; // Переход на главную страницу
+// // });
+// //
+// // document.getElementById('albumButton').addEventListener('click', function() {
+// //   window.location.href = 'album.html'; // Переход на страницу контактов
+// // });
+//
+// // выделение элемента по нажатию
+// sidebarItems.forEach((sideItem) => {
+//   sideItem.addEventListener('click', () => {
+//     sidebarItems.forEach((item) => {
+//       item.classList.remove('active')
+//     })
+//     sideItem.classList.add('active')
+//   })
+// })
+//
+// // для маленьких девайсов
+// window.addEventListener('scroll', () => {
+//   if (window.innerWidth <= 992) {
+//     if (window.pageYOffset > 20) {
+//       sidebar.style.paddingTop = '20px'
+//     } else {
+//       sidebar.style.paddingTop = '70px'
+//     }
+//   }
+// })
+//
+// // выделение нужной категории при нужной странице
+// document.addEventListener('DOMContentLoaded', () => {
+//   const currentPath = window.location.pathname
+//   console.log('Current Path:', currentPath) // отображение страницы текущей
+//
+//   sidebarItems.forEach((item) => {
+//     item.classList.remove('active')
+//   })
+//
+//   if (currentPath.includes('album.html')) {
+//     const popularTab = document.querySelector('.sidebar .item:nth-child(3)')
+//     console.log('Popular Tab:', popularTab)
+//     if (popularTab) {
+//       popularTab.classList.add('active')
+//       console.log('Popular Tab Activated')
+//     }
+//   } else if (currentPath.includes('index.html') || currentPath === '/') {
+//     const homeTab = document.querySelector('.sidebar .item:nth-child(1)')
+//     console.log('Home Tab:', homeTab)
+//     if (homeTab) {
+//       homeTab.classList.add('active')
+//       console.log('Home Tab Activated')
+//     }
+//   }
+// })// const sidebar = document.querySelector('.left-section')
+// const sidebarItems = document.querySelectorAll('.sidebar .item')
+// const catItems = document.querySelectorAll('.categories a')
+//
+// // document.getElementById('homeButton').addEventListener('click', function() {
+// //   window.location.href = 'index.html'; // Переход на главную страницу
+// // });
+// //
+// // document.getElementById('albumButton').addEventListener('click', function() {
+// //   window.location.href = 'album.html'; // Переход на страницу контактов
+// // });
+//
+// // выделение элемента по нажатию
+// sidebarItems.forEach((sideItem) => {
+//   sideItem.addEventListener('click', () => {
+//     sidebarItems.forEach((item) => {
+//       item.classList.remove('active')
+//     })
+//     sideItem.classList.add('active')
+//   })
+// })
+//
+// // для маленьких девайсов
+// window.addEventListener('scroll', () => {
+//   if (window.innerWidth <= 992) {
+//     if (window.pageYOffset > 20) {
+//       sidebar.style.paddingTop = '20px'
+//     } else {
+//       sidebar.style.paddingTop = '70px'
+//     }
+//   }
+// })
+//
+// // выделение нужной категории при нужной странице
+// document.addEventListener('DOMContentLoaded', () => {
+//   const currentPath = window.location.pathname
+//   console.log('Current Path:', currentPath) // отображение страницы текущей
+//
+//   sidebarItems.forEach((item) => {
+//     item.classList.remove('active')
+//   })
+//
+//   if (currentPath.includes('album.html')) {
+//     const popularTab = document.querySelector('.sidebar .item:nth-child(3)')
+//     console.log('Popular Tab:', popularTab)
+//     if (popularTab) {
+//       popularTab.classList.add('active')
+//       console.log('Popular Tab Activated')
+//     }
+//   } else if (currentPath.includes('index.html') || currentPath === '/') {
+//     const homeTab = document.querySelector('.sidebar .item:nth-child(1)')
+//     console.log('Home Tab:', homeTab)
+//     if (homeTab) {
+//       homeTab.classList.add('active')
+//       console.log('Home Tab Activated')
+//     }
+//   }
+// })
 // const sidebar = document.querySelector('.left-section')
 // const sidebarItems = document.querySelectorAll('.sidebar .item')
 // const catItems = document.querySelectorAll('.categories a')
